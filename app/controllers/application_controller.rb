e06bb6580fc4c703ed88f9f96d88923e5d00a3d5
abe227 <absn0227@gmail.com>
2022-11-14 18:17:21 +0900

    ユーザー管理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :first_name, :kana_surname, :kana_first_name, :birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :password, presence: true
+         validates :encrypted_password, presence: true
+         validates :email, presence: true
+         validates :surname, presence: true
+         validates :first_name, presence: true
+         validates :kana_surname, presence: true
+         validates :kana_first_name, presence: true
+end
