c6654ff74dcb08a8e2fb1254c0fabc7d9592e691
abe227 <absn0227@gmail.com>
2022-11-22 13:17:22 +0900

    修正2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,32 +32,51 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = 'あ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '価格が300円未満では出品できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '価格が9_999_999円を超えると出品できない' do
+        @item.price = '11111111'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+
       it 'category_idが空だと登録できない' do
-        @item.category_id = ''
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it 'situation_idが空だと登録できない' do
-        @item.situation_id = ''
+        @item.situation_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Situation can't be blank")
       end
 
       it 'cost_idが空だと登録できない' do
-        @item.cost_id = ''
+        @item.cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Cost can't be blank")
       end
 
       it 'days_to_ship_idが空だと登録できない' do
-        @item.days_to_ship_id = ''
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
 
       it 'prefecture_idが空だと登録できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
