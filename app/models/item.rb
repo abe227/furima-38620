b47d67a9783935e2ed307bee293205a8d3fc76b3
abe227 <absn0227@gmail.com>
2022-12-05 11:32:40 +0900

    修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :explanation, presence: true
-  validates :price, presence: true, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category_id, presence: true, numericality: { other_than: 1 ,  } 
   validates :situation_id, presence: true, numericality: { other_than: 1 ,  }
   validates :cost_id, presence: true, numericality: { other_than: 1 , }
